5f18549edb2079c6e05d68ddae87a26db6085fd1
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-21 17:24:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,9 +11,10 @@ def find_max_value(array)
   x = 0  
     array.length.times {|index|
     if array[index] > x
-      x = array 
+      x = array[index]
     end
     }
+    x
 end
 
 def find_min_value(array)
