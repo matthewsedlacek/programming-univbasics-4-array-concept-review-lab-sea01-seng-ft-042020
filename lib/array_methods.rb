dbb12f2a4d1bd9bf5ff5452d56afa96dcc3a8681
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-21 17:11:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
     array.length.times {|count|
     if array[count] = value_to_find
-    return count
+      return count
     }
-
+    
 def find_max_value(array)
   
 end
